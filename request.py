81399a7becba45369b5d48f23fd4a3a379143b14
Enrique Avina <enriqueavina@Enriques-MacBook-Air-750.local>
2019-04-22 18:32:59 -0400

    Added POST request handling logic

--- request.py
+++ request.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append("__HOME__/finalProject")
 
 def request_handler(request):
-	
+
 	if request["method"] == "GET":
 		example_db = "__HOME__/finalProject/players.db" # just come up with name of database
 		conn = sqlite3.connect(example_db)  # connect to that database (will create if it doesn't already exist)
@@ -16,28 +16,33 @@ def request_handler(request):
 		conn.close() # close connection to databa
 		return things
 
-
 	if request["method"] == "POST":
-
-		data = request["form"]
-		ID = int(data["ID"])
-		health = int(data["health"])
-		action = data["action"]
-
 		example_db = "__HOME__/finalProject/players.db" # just come up with name of database
 		conn = sqlite3.connect(example_db)  # connect to that database (will create if it doesn't already exist)
 		c = conn.cursor()  # make cursor into database (allows us to execute commands)
 		c.execute('''CREATE TABLE IF NOT EXISTS playerData (ID int, health int);''') # run a CREATE TABLE command
+
+		'''
+		Make table for timers (text, timestamp)
+		'''
+		
+		data = request["form"]
+
+		# every request should have an ID and an action
+		try:
+			ID = int(data["ID"])
+			action = data["action"]
+		except:
+			return "Incorrect POST request"
+
+		# handle specific POST Requests
+		if (action == "initial"):
+			startGame(c)
+
 		if(action == "initial"):
-			things =  c.execute('''SELECT * FROM playerData;''').fetchall()
-			if not (ID,health) in things:
-				c.execute('''INSERT into playerData VALUES (?,?);''',(ID,health))
-		else:
-
+			health = int(data["health"])
 			prev = c.execute('''SELECT * FROM playerData WHERE ID=(?);''',(ID))
-
 			c.execute('''DELETE FROM playerData WHERE ID=(?);''',(ID))
-
 			c.execute('''INSERT into playerData VALUES (?,?);''',(ID,prev[1]-1))
 
 		conn.commit() # commit commands
@@ -46,3 +51,10 @@ def request_handler(request):
 
 	# {'method': 'POST', 'args': [], 'values': {}, 'is_json': False, 'form': {'ID': '1', 'health': '5', 'action': 'initial'}}
 	return request
+
+def startGame(c):
+	things =  c.execute('''SELECT * FROM playerData;''').fetchall()
+	# add players to the database
+
+	if not (ID,health) in things:
+		c.execute('''INSERT into playerData VALUES (?,?);''',(ID,health))
